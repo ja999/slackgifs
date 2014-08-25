33601d40147d676438b0ee67b5b752ff8405f4d8
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2014-08-25 03:02:06 +0200

    Add wrapper methods for GiphyParser to Gif model

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,2 +1,12 @@
 class Gif < ActiveRecord::Base
+
+  def self.get_urls(json_string)
+    response_parser = ::GiphyResponseParser.new(json_string)
+    response_parser.original_urls
+  end
+
+  def self.get_url(json_string, id)
+    response_parser = ::GiphyResponseParser.new(json_string)
+    response_parser.original_url(id)
+  end
 end
