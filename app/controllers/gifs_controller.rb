7089189ca7697cd41ad4412a9f85ba8c647cb85d
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2014-08-25 03:01:41 +0200

    Add index and dummy search to GifsController

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,2 +1,20 @@
 class GifsController < ApplicationController
+  before_filter :get_required
+
+  def index
+    # TODO: Render something else than Ryan Goslling gifs. ;)
+    url = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dc6zaTOxFJmzC&limit=5"
+    response = Net::HTTP.get_response(URI.parse(url)).body
+    render text: Gif.get_urls(response)
+  end
+
+  def search
+    render json: []
+  end
+
+  private
+
+  def get_required
+    require 'net/http'
+  end
 end
